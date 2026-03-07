3f9a00627ed06fcc9a0bbbef170996ac8ff8afe8
PREMIUMUPVC <kspremiumupvc@gmail.com>
2026-03-07 19:17:37 +0530

    Update replace-env.sh

--- replace-env.sh
+++ replace-env.sh
@@ -1,7 +1,8 @@
-find . -type f -name "*.html" -exec sed -i "s|__API_KEY__|$API_KEY|g" {} +
-find . -type f -name "*.html" -exec sed -i "s|__AUTH_DOMAIN__|$AUTH_DOMAIN|g" {} +
-find . -type f -name "*.html" -exec sed -i "s|__PROJECT_ID__|$PROJECT_ID|g" {} +
-find . -type f -name "*.html" -exec sed -i "s|__STORAGE_BUCKET__|$STORAGE_BUCKET|g" {} +
-find . -type f -name "*.html" -exec sed -i "s|__MESSAGING_SENDER_ID__|$MESSAGING_SENDER_ID|g" {} +
-find . -type f -name "*.html" -exec sed -i "s|__APP_ID__|$APP_ID|g" {} +
-find . -type f -name "*.html" -exec sed -i "s|__MEASUREMENT_ID__|$MEASUREMENT_ID|g" {} +
+sed -i "s|__API_KEY__|$API_KEY|g" *.html
+sed -i "s|__AUTH_DOMAIN__|$AUTH_DOMAIN|g" *.html
+sed -i "s|__PROJECT_ID__|$PROJECT_ID|g" *.html
+sed -i "s|__STORAGE_BUCKET__|$STORAGE_BUCKET|g" *.html
+sed -i "s|__MESSAGING_SENDER_ID__|$MESSAGING_SENDER_ID|g" *.html
+sed -i "s|__APP_ID__|$APP_ID|g" *.html
+sed -i "s|__MEASUREMENT_ID__|$MEASUREMENT_ID|g" *.html
+sed -i "s|__ADMIN_EMAIL__|$ADMIN_EMAIL|g" *.html
